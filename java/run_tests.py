c5ca794634a7abbdb41375d1120008082c5e5a3e
Lehnart <thomasserre63@gmail.com>
2025-10-20 14:05:57 +0200

    Add a common framework for tests

---
+++ java/run_tests.py
@@ -0,0 +1,38 @@
+import subprocess
+import os 
+import pathlib
+
+os.chdir("problems")
+
+problems = os.listdir()
+
+for problem in problems :
+    print("Running tests for " + problem)
+    os.chdir(problem)
+
+    if not os.path.isdir("res"):
+        os.chdir("..")
+        continue 
+
+    test_files = os.listdir("res")
+    inputs = [f for f in test_files if f.startswith("input")]
+    outputs = [f for f in test_files if f.startswith("output")]
+    inputs.sort()
+    outputs.sort()
+
+    for index in range(len(inputs)):
+        input_file = pathlib.Path("res", inputs[index])
+        output_file = pathlib.Path("res", outputs[index])
+        result_file = "result.txt"
+
+        with open(input_file, "r") as f, open(result_file, "w") as r :
+            subprocess.run("java java\\Main.java", stdin=f, stdout=r)
+
+        with open(output_file, "r") as o, open(result_file, "r") as r :
+            output_content = o.read()
+            result_content = r.read()
+            assert output_content == result_content
+
+        os.remove("result.txt")
+
+    os.chdir("..")
