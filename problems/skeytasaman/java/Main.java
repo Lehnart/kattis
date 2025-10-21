03f194d42717b9351a58d63a68ae5a11611189c7
Lehnart <thomasserre63@gmail.com>
2025-10-21 14:33:44 +0200

    Solve 5 problems

---
+++ problems/skeytasaman/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String data = sc.next();
+        String data2 = sc.next();
+        sc.close();
+        System.out.println(data+data2);
+    }
+}
