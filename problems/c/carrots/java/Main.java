f2833c1c5a907a63651a8b29c66728bdb5f90f7c
Lehnart <thomasserre63@gmail.com>
2025-10-25 09:56:30 +0200

    add carrot problem solution

---
+++ problems/c/carrots/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        int carrots = sc.nextInt();
+        for(int i = 0; i<c; i++){
+            String d = sc.next();
+        }
+        sc.close();
+        System.out.println(carrots);
+    }
+}
