9009d8b39a8206f8b969191ed24998a914d903f7
Lehnart <thomasserre63@gmail.com>
2025-11-08 11:30:38 +0100

    one elftover problem

---
+++ problems/a/abovesealevel/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        sc.close();
+        System.out.println(a-0.3);
+    }
+}
