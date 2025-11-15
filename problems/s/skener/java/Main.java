db80fe5105e5505148bc252e03ec5ccffc4444f0
Lehnart <thomasserre63@gmail.com>
2025-11-15 08:38:51 +0100

    correct skener

--- problems/s/skener/java/Main.java
+++ problems/s/skener/java/Main.java
@@ -7,11 +7,17 @@ public class Main {
         int c = sc.nextInt();
         int zr = sc.nextInt();
         int zc = sc.nextInt();
+        sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+        StringBuilder lineBuilder = new StringBuilder();
         for(int i = 0; i < r; i++){
+            String line = sc.nextLine().strip();
             for(int j =0; j < c; j++){
-                
+                for(int k = 0 ; k < zc; k++) lineBuilder.append(line.charAt(j));
             }
+            for(int l = 0; l < zr; l++) sb.append(lineBuilder.toString() + "\n");
+            lineBuilder = new StringBuilder();
         }
-
+        System.out.print(sb.toString());
     }
 }
